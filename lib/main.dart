e5ba51bab404b2b5bd273f48f8dcc838158e55af
15ucs025 <15ucs025@gmail.com>
2022-12-07 16:33:04 +0530

    Added dependency for firebase auth integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_auth_login/Screens/homeView.dart';
 import 'package:firebase_auth_login/Screens/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
